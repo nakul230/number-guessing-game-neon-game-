138d5650c33b3230d167b4359014f695550debcf
nakul230 <nakulsyryavanshi882@gmail.com>
2026-04-19 20:56:23 +0530

    deployment

--- app.py
+++ app.py
@@ -285,6 +285,8 @@ def guess(num):
         return jsonify({"message": f"🏆 MISSION SUCCESS! FOUND IN {attempts} TRIES.", "result": "win", "attempts": attempts})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
 
     #false 
